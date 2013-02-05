6545b6ef877589b0044612701cbb1e7e89ba6eba
Casey Lang <clang1234@gmail.com>
2013-02-04 21:25:15 -0600

    Fix recipe gathering
    * Fix filter query
    * get entire note instead of just metadata

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,12 +8,17 @@ class RecipesController < ApplicationController
       filter = Evernote::EDAM::NoteStore::NoteFilter.new
       constants = Evernote::EDAM::Type
       filter.order = constants::NoteSortOrder::UPDATED
-      filter.words = "any: classifications_recipe:#{constants::CLASSIFICATION_RECIPE_SERVICE_RECIPE}" +
+      filter.words = "any: classifications_recipe:#{constants::CLASSIFICATION_RECIPE_SERVICE_RECIPE} " +
           "classifications_recipe:#{constants::CLASSIFICATION_RECIPE_USER_RECIPE}"
       spec = Evernote::EDAM::NoteStore::NotesMetadataResultSpec.new
       spec.includeTitle = true
 
-      @recipes = note_store.findNotesMetadata(auth_token, filter, 0, 100, spec).notes
+      notes = note_store.findNotesMetadata(auth_token, filter, 0, 100, spec).notes
+
+      @recipes = []
+      notes.each do |note|
+        @recipes << note_store.getNote(auth_token, note.guid, true, true, false, false)
+      end
     end
   end
 end
