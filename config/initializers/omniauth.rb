f5a1e839d224a6744422c74997e06c5def2dd19b
Casey Lang <clang1234@gmail.com>
2013-02-13 15:20:20 -0600

    Make demo user yml pass through ERB parser first.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,9 +5,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            :client_options => { :site => 'https://sandbox.evernote.com' }
 end
 
-if Rails.env.production?
+if Rails.env.production? && ENV['DEMO_MODE']
   # Turn on 'test mode' for OmniAuth when in production for demo
   OmniAuth.config.test_mode = true
-  evernote_yml = YAML.load_file(Rails.root.join("config/demo/evernote_demo_user.yml"))
+  demo_config = Rails.root.join("config/demo/evernote_demo_user.yml")
+  evernote_yml = YAML.load(ERB.new(File.read(demo_config)).result)
   OmniAuth.config.mock_auth[:evernote] = OmniAuth::AuthHash.new evernote_yml
 end
